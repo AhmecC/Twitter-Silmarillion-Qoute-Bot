a4419316a101bd4c72974c3e765a41bb35089194
Aqeel Ahmed <aqeelvahmed@gmail.com>
2022-07-30 14:01:19 +0100

    Update SilmarillionQoutes.py

--- SilmarillionQoutes.py
+++ SilmarillionQoutes.py
@@ -1,4 +1,5 @@
 import tweepy
+import schedule
 import random
 from time import sleep
 from imageCreator import imageCreator  # Imports class that creates the image
@@ -23,34 +24,41 @@ def formatter(CHOSEN):
     quote = f"'{text[:-1]}'"
     return quote
 
-START = True
-while START:
+def main():
     CHOICE = f"Chap {random.randint(1,28)}"  # Chooses Chap where Quote will come from
 
     with open(f"Silmarillion/{CHOICE}/{CHOICE}.txt", encoding="utf-8") as file:
         quotes = file.readlines()
         CHOSEN = random.choice(quotes)
         quotes.remove(CHOSEN)  # Chooses a Quote from specified chapter and then deletes it from file
-
     with open(f"Silmarillion/{CHOICE}/{CHOICE}.txt","w", encoding="utf-8") as f:
         for i in quotes:
             f.write(i)
-
 
     names = ["Durin", "Elrond", "Galadriel", "Númenor", "Lindon", "Isildur", "Gil-galad", "Elendil", "Finrod", "Ar-Pharazôn", "Sauron"] 
       hash = ["#TheRingsOfPower", "#amazon", "#LordoftheRings", "#tolkien"]  # If relevant name is found in quote is created into a hashtag
     for i in names:
         if i in CHOSEN:
             hash.append(f"#{i}")
-
-
     CHOSEN = formatter(CHOSEN)
-    
     a = imageCreator(CHOSEN, CHOICE)  # Creates the image itself
-
     media = api.media_upload("result.png")
     hashtags = " ".join(hash)
     tweet = api.update_status(status=hashtags, media_ids=[media.media_id])
     api.create_favorite(id=tweet.id)
+    
+    
+# -------------------- SCHEDULER -------------------- #
+schedule.every().day.at("08:00").do(main)
+schedule.every().day.at("10:00").do(main)
+schedule.every().day.at("12:00").do(main)
+schedule.every().day.at("14:00").do(main)
+schedule.every().day.at("16:00").do(main)
+schedule.every().day.at("18:00").do(main)
+schedule.every().day.at("20:00").do(main)
+schedule.every().day.at("22:00").do(main)
 
-    sleep(10800)  # Every 3 Hours
+while True:
+    schedule.run_pending()
+    sleep(1)
+    
